86372cd1b42fd896e83439ce430c69342998ab4e
Patrick Fornatto <juveazzurri1432@gmail.com>
2025-04-23 19:41:08 -0500

    Changes

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import flask
+from flask import Flask, render_template
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return ("Hello World")
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5003, debug=True)
+if __name__ == '__main__':
+    app.run(port=5003, debug=True)
